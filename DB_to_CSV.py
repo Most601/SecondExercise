dc767086cad176ce4336cb01f12a18760d96159e
MariaVinog <maria.vinog@gmail.com>
2016-05-07 14:42:46 +0300

    Update DB_to_CSV.py
    add comments.

--- DB_to_CSV.py
+++ DB_to_CSV.py
@@ -6,10 +6,10 @@ from datetime import datetime
 import math
 
 
-
+#This function operates and opens the database and writing to CSV file.
 def toCSV():
-    conn = sqlite3.connect("nmea_to_db.db") #open db
-    cursor = conn.cursor() #cursor to the db
+    conn = sqlite3.connect("nmea_to_db.db")
+    cursor = conn.cursor()
     INPUT = 'NMEAFiles'
     if os.path.isdir(INPUT):
         l = os.listdir(INPUT)
@@ -18,7 +18,7 @@ def toCSV():
             listName = spl.split('.')
             cursor.execute('SELECT * FROM '+str(listName[0]) )
             print "[ '"+str(listName[0])+"' , CSV FILE ]"
-            with open("CSV\\"+listName[0]+".csv", "wb") as csv_file: #writing to csv
+            with open("CSV\\"+listName[0]+".csv", "wb") as csv_file:
                 csv_writer = csv.writer(csv_file)
-                csv_writer.writerow([i[0] for i in cursor.description]) # write headers
+                csv_writer.writerow([i[0] for i in cursor.description])
                 csv_writer.writerows(cursor)
